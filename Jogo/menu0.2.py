9dc395d2eead5b9a11ffdbe54151b403bb976500
samaramilena.cc <samaramilena.cc@gmail.com>
2010-11-27 00:01:41 -0200

    Primeira vers?o do jogo inserido no menu. ( Ainda em Constru??o )

---
+++ Jogo/menu0.2.py
@@ -0,0 +1,238 @@
+# -*- coding: utf-8 -*-
+#
+# autor: Renato Cesar Vieira
+# web: pythonbr.superforo.net
+# licencia: GPL 2
+
+import random
+import os
+import pygame
+from pygame.locals import *
+import sys
+from gardenmined import Main
+
+class Opcao:
+
+    def __init__(self, fonte, titulo, x, y, paridad, funcao_asignada):
+        self.imagen_normal = fonte.render(titulo, 1, (255, 69, 0))
+        self.imagen_destacada = fonte.render(titulo, 1, (200, 0, 0))
+        self.image = self.imagen_normal
+        self.rect = self.image.get_rect()
+        self.rect.x = 500 * paridad
+        self.rect.y = y
+        self.funcao_asignada = funcao_asignada
+        self.x = float(self.rect.x)
+
+    def atualizar(self):
+        destino_x = 295
+        self.x += (destino_x - self.x) / 5.0
+        self.rect.x = int(self.x)
+
+    def imprimir(self, screen):
+        screen.blit(self.image, self.rect)
+
+    def destacar(self, estado):
+        if estado:
+            self.image = self.imagen_destacada
+        else:
+            self.image = self.imagen_normal
+
+    def ativar(self):
+        self.funcao_asignada()
+
+class Cursor:
+
+    def __init__(self, x, y, dy):
+        self.image = pygame.image.load("Menu" + os.sep + "cursor1.png").convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.y_inicial = y
+        self.dy = dy
+        self.y = 0
+        self.selecionar(0)
+
+    def atualizar(self): 
+        self.y += (self.to_y - self.y) / 1.0# aumenta ou diminui a velocidade do cursor
+        self.rect.y = int(self.y)
+
+    def selecionar(self, indice):
+        self.to_y = self.y_inicial + indice * self.dy
+
+    def imprimir(self, screen):
+        screen.blit(self.image, self.rect)
+
+
+class Menu:
+    "Representa um menu com opcoes para um jogo"
+    
+    def __init__(self, opciones):
+        self.opcoes = []
+        fonte = pygame.font.Font("Menu" + os.sep + "SABRINAS.ttf", 35)
+        x = 335 # Alinha a direita ou equerda o cursor
+        y = 150 # Dece ou sobe as opcoes
+        paridad = 1
+
+        self.cursor = Cursor(x - 69, y + 15, 50)
+
+        for titulo, funcao in opcao:
+            self.opcoes.append(Opcao(fonte, titulo, x, y, paridad, funcao))
+            y += 50
+            if paridad == 1:
+                paridad = -1
+            else:
+                paridad = 1
+
+        self.selecionado = 0
+        self.total = len(self.opcoes)
+        self.mantem_pulsado = False
+
+    def atualizar(self):
+        """Altera o valor de 'self.selecionado' com os direcoes."""
+
+        k = pygame.key.get_pressed()
+
+        if not self.mantem_pulsado:
+            if k[K_UP]:
+                self.selecionado -= 1
+            elif k[K_DOWN]:
+                self.selecionado += 1
+            elif k[K_RETURN]:
+                # Invoca a funcao associada a opcao.
+                self.opcoes[self.selecionado].ativar()
+
+        # verifica se o cursor esta entrelas opcoes permitidas.
+        if self.selecionado < 0:
+            self.selecionado = 0
+        elif self.selecionado > self.total - 1:
+            self.selecionado = self.total - 1
+        
+        self.cursor.selecionar(self.selecionado)
+
+        # indica se o usuario mantem pulsada alguna tecla.
+        self.mantem_pulsado = k[K_UP] or k[K_DOWN] or k[K_RETURN]
+
+        self.cursor.atualizar()
+     
+        for o in self.opcoes:
+            o.atualizar()
+
+    def imprimir(self, screen):
+        """Imprime sobre 'screen' o texto de cada opcao do menu."""
+
+        self.cursor.imprimir(screen)
+
+        for opcao in self.opcoes:
+            opcao.imprimir(screen)
+            
+def comecar_novo_jogo():
+    Main()
+    sair = False
+    pygame.font.init()
+    pygame.display.set_caption ("Garden Minde.")
+    screen = pygame.display.set_mode ((800, 600))
+    fundo = pygame.image.load("Menu" + os.sep + "jogar.png").convert()
+    
+    while not sair:
+        tecla = pygame.key.get_pressed()
+        for e in pygame.event.get():
+            if e.type == QUIT:
+                sair = True
+
+            if tecla[pygame.K_ESCAPE]:
+                sair = True
+
+        screen.blit(fundo, (0, 0))
+        pygame.display.flip()
+        pygame.time.delay(10)
+
+def instrucoes():
+    sair = False
+    pygame.font.init()
+    pygame.display.set_caption ("Garden Minde.")
+    screen = pygame.display.set_mode ((800, 600))
+    fundo = pygame.image.load("Menu" + os.sep + "instrucao.png").convert()
+    
+    while not sair:
+        tecla = pygame.key.get_pressed()
+        for e in pygame.event.get():
+            if e.type == QUIT:
+                sair = True
+
+            if tecla[pygame.K_ESCAPE]:
+                sair = True
+
+        screen.blit(fundo, (0, 0))
+        pygame.display.flip()
+        pygame.time.delay(10)
+
+def creditos():
+    sair = False    
+    pygame.font.init()
+    pygame.display.set_caption ("Garden Mined.")
+    screen = pygame.display.set_mode ((800, 600))
+    fundo = pygame.image.load("Menu" + os.sep + "creditos.png").convert()
+    
+    while not sair:
+        tecla = pygame.key.get_pressed()
+        for e in pygame.event.get():
+            if e.type == QUIT:
+                sair = True
+            if tecla[pygame.K_ESCAPE]:
+                sair = True
+
+        screen.blit(fundo, (0, 0))
+        pygame.display.flip()
+        pygame.time.delay(10)
+
+def sair_do_jogo():
+    sair = False
+    pressionou = False
+    pygame.font.init()
+    fundo = pygame.image.load("Menu" + os.sep + "sair_jogo.png").convert()
+    pygame.time.delay(800)
+
+    while not sair:
+        tecla = pygame.key.get_pressed()
+        for e in pygame.event.get():
+            if e.type == QUIT:
+                sair = True
+
+            if tecla[pygame.K_ESCAPE]:
+                sair = True
+
+            if pressionou:
+                sys.exit(0)
+
+            if tecla[K_RETURN]:
+                pressionou = True
+                
+        screen.blit(fundo, (200, 200))
+        pygame.display.flip()
+        pygame.time.delay(10)
+        
+if __name__ == '__main__':
+    
+    sair = False
+    opcao = [
+        ("Jogar", comecar_novo_jogo),
+        ("Instrucoes", instrucoes),
+        ("Creditos", creditos),
+        ("Sair do Jogo", sair_do_jogo)
+        ]
+
+    pygame.font.init()
+    screen = pygame.display.set_mode((800, 600))
+    fundo = pygame.image.load("Menu" + os.sep + "fundo_menu.png").convert()
+    menu = Menu(opcao)
+
+    while not sair:
+
+        for e in pygame.event.get():
+            if e.type == QUIT:
+                sair = True
+
+        screen.blit(fundo, (0, 0))
+        menu.atualizar()
+        menu.imprimir(screen)
+        pygame.display.flip()
+        pygame.time.delay(10)
--- Jogo/novo.py
+++ Jogo/novo.py
@@ -10,54 +10,43 @@ def multiimageload(images):
         list.append(pygame.image.load(image))
     return list
 
-MEDIUM = pygame.font.Font("resources" + os.sep + "CHICK___.ttf", 24)
+MEDIUM = pygame.font.Font("resources" + os.sep + "Samson.ttf", 24)
 
 #Carregando imagens
-
-#img_marker = pygame.image.load("resources" + os.sep + "img_marker.png")
+img_marker = pygame.image.load("resources" + os.sep + "img_marker.png")
 img_flag = pygame.image.load("resources" + os.sep + "img_flor1.png")
-img_bombsleft = MEDIUM.render("bomba left: ", True, (255, 255, 255))
-img_spaces = multiimageload(["resources" + os.sep + "img_1.png", "resources" + os.sep + "img_2.png", "resources" + os.sep + "img_3.png", "resources" + os.sep + "img_4.png", "resources" + os.sep + "img_5.png", "resources" + os.sep + "img_6.png", "resources" + os.sep + "img_7.png", "resources" + os.sep + "img_8.png", "resources" + os.sep + "img_bomba1.png"])
+img_bombsleft = MEDIUM.render("Bombs left: ", True, (255, 255, 255))
+img_spaces = multiimageload(["resources" + os.sep + "img_fundo.png", "resources" + os.sep + "img_1.png", "resources" + os.sep + "img_2.png", "resources" + os.sep + "img_3.png", "resources" + os.sep + "img_4.png", "resources" + os.sep + "img_5.png", "resources" + os.sep + "img_6.png", "resources" + os.sep + "img_7.png", "resources" + os.sep + "img_8.png", "resources" + os.sep + "img_bomba1.png"])
 som_bomba = pygame.mixer.Sound("resources" + os.sep + "explode.wav")
-img_fundoo = pygame.image.load("resources" + os.sep + "a.png")
-#img_fundo2 = pygame.image.load("resources" + os.sep + "img_fundooo.png")
-som_on = pygame.image.load("resources" + os.sep + "Som.png")
-#som_off = pygame.image.load("resources" + os.sep + "som_off.png")
-#img_bombsleft = MEDIUM.render("Bombs left: ", True, (255, 255, 255))
-#img_youwin = MEDIUM.render("Parabens Voce Venceu!", True, (0, 255, 0))
-#img_youlose = MEDIUM.render("Nao foi dessa vez =(", False, (255, 0, 0))
-voltar_menu = pygame.image.load("resources" + os.sep + "seta_voltar.png")
-bordas = pygame.image.load("resources" + os.sep + "bordas.png")
-nome = pygame.image.load("resources" + os.sep + "nome.png")
-
+img_fundoo = pygame.image.load("resources" + os.sep + "fundo.png")
 
 class Game(object):
-    def __init__(self, m, n, bomba):
-        self.numero_bombas = bomba
-        self.bombsleft = bomba
+    def __init__(self, m, n, bombs):
+        self.bombsnumber = bombs
+        self.bombsleft = bombs
         self.img_bombsleft = MEDIUM.render(str(self.bombsleft), True, (255, 255, 255))
         self.m = m
         self.n = n
-        self.todiscover = m * n - bomba
+        self.todiscover = m * n - bombs
         self.board = []
+        self.started = False
+        self.seenboard = []
         for b in range(self.n):
             self.board.append([])
         for b in range(self.n):
             for a in range(self.m):
                 self.board[b].append(0)
-        self.seenboard = []
         for b in range(self.n):
             self.seenboard.append([])
         for b in range(self.n):
             for a in range(self.m):
                 self.seenboard[b].append(0)
-        self.started = False
-
+        
 # Montagem dos quadradinhos
 
     def start(self, x, y):
         self.started = True
-        for bomb in range(self.numero_bombas):
+        for bomb in range(self.bombsnumber):
             free = False
             while not free:
                 a = random.randint(0, self.m - 1)
@@ -115,6 +104,8 @@ class Game(object):
                 if y < self.n - 1:
                     self.discover(x, y+1)
 
+# Configuracoes da bandeirinha ( flor )
+
     def flag(self, x, y):
         if self.seenboard[x][y] == 0:
             self.seenboard[x][y] = -1
@@ -127,43 +118,35 @@ class Game(object):
 
 class Main(object):
     def __init__(self):
-        self.startgame(15, 15, 30, "PyBomb - Easy")
+        self.startgame(15, 15, 5, "PyBomb - Very Easy")
         self.mouseleft = "up"
         self.mouseright = "up"
         self.mode = 1
         self.clock = pygame.time.Clock()
         self.loop()
-    def startgame(self, m, n, bomba, legenda):
+    def startgame(self, m, n, bombs, caption):
         self.mode = 1
-        self.game = Game(m, n, bomba)
-        self.width= m * 33 + 220
-        self.height = n * 33 + 90 
-        self.ganhardow = pygame.display.set_mode((self.width, self.height))
-        pygame.display.set_caption(legenda)
-        self.screen = screen = pygame.display.set_mode((self.width,self.height))
-
-    def ganhar(self):
+        self.game = Game(m, n, bombs)
+        self.width = m * 33 + 120
+        self.height = n * 33 + 30
+        self.window = pygame.display.set_mode((self.width, self.height))
+        pygame.display.set_caption(caption)
+        self.screen = pygame.display.get_surface()
+    def win(self):
         self.mode = 2
         for a in range(self.game.m):
             for b in range(self.game.n):
                 if self.game.seenboard[a][b] == 0:
                     if self.game.board[a][b] == -1:
                         self.game.seenboard[a][b] = -1
-    def perder(self):
+    def lose(self):
         self.mode = 3
         for a in range(self.game.m):
             for b in range(self.game.n):
                 if self.game.seenboard[a][b] == 0:
                     if self.game.board[a][b] == -1:
                         self.game.seenboard[a][b] = 1
-                    
     def loop(self):
-        self.screen.blit(nome, (100,500))
-        self.screen.blit(voltar_menu, (0, 350))
-        self.screen.blit(som_on, (0, 150))
-        self.screen.blit(bordas, (550, 50))
-        #self.screen.blit(img_fundoo, (0,0))
-        
         while True:
             self.clock.tick(30)
             self.input(pygame.event.get())
@@ -178,14 +161,11 @@ class Main(object):
             self.mouseleft = "down"
         if self.mouseright == "pressed":
             self.mouseright = "down"
-        
         for event in events:
-            if event.type == QUIT or (event.type == KEYDOWN and event.key == 27):
+            if event.type == QUIT or (event.type == KEYDOWN and event.key ==27):
                 pygame.time.delay(100)
                 sys.exit(0)
             (self.mouse_x, self.mouse_y) = pygame.mouse.get_pos()
-
-
             if event.type == MOUSEBUTTONDOWN:
                 if event.button == 1:
                     self.mouseleft = "pressed"
@@ -199,46 +179,58 @@ class Main(object):
 
     def action(self):
         if self.mouseleft == "pressed":
-            
-    
+           
             if self.mode == 1:
-                if (self.mouse_x in range(64, 64 + self.game.m * 32)): 
-                     if (self.mouse_y in range(32, 32 + self.game.n * 32)): 
+                if (self.mouse_x in range(0, self.game.m * 32)):
+                    if (self.mouse_y in range(32, 32 + self.game.n * 32)):
                         if self.game.started == False:
-                            if self.game.seenboard[(self.mouse_x -64) // 32][(self.mouse_y - 32) // 32] == 0: 
-                                self.game.start((self.mouse_x - 64) // 32, (self.mouse_y - 32) // 32) 
-                        if self.game.discover((self.mouse_x - 64 )// 32, (self.mouse_y - 32) // 32) == "Bomb":
+                            if self.game.seenboard[self.mouse_x // 32][(self.mouse_y - 32) // 32] == 0:
+                                self.game.start(self.mouse_x // 32, (self.mouse_y - 32) // 32)
+                        if self.game.discover(self.mouse_x // 32, (self.mouse_y - 32) // 32) == "Bomb":
                             som_bomba.play()
-                            self.perder()
+                            self.lose()
                         if self.game.todiscover == 0:
-                            self.ganhar()
+                            self.win()
         if self.mouseright == "pressed":
             if self.mode == 1:
-                if (self.mouse_x in range(0, 64 + self.game.m * 64)):
-                    if (self.mouse_y in range(0, 32 + self.game.n * 32)):
-                        self.game.flag((self.mouse_x - 64) // 32, (self.mouse_y - 32) // 32)
-        
-                                
+                if (self.mouse_x in range(10, self.game.m * 32)):
+                    if (self.mouse_y in range(32, 32 + self.game.n * 32)):
+                        self.game.flag(self.mouse_x // 32, (self.mouse_y - 32) // 32)
+                       
     def draw(self):
-        screen = self.screen          
-                    
-        #if self.mode == 2:
-            #screen.blit(img_youwin, (4, 4))
-        #elif self.mode == 3:
-            #screen.blit(img_youlose, (4, 4))
-        
+        screen = self.screen
+       
+        if self.mouse_x in range((self.width - 120/2), (self.width/2)):
+                if self.mouse_y in range(12, 36):
+                    pygame.draw.rect(screen, (0, 0, 0), (self.width - 119, 12, 118, 24), 1)
+                if self.mouse_y in range(44, 68):
+                    pygame.draw.rect(screen, (0, 0, 0), (self.width - 119, 44, 118, 24), 1)
+                if self.mouse_y in range(76, 100):
+                    pygame.draw.rect(screen, (0, 0, 0), (self.width - 119, 76, 118, 24), 1)
+                if self.mouse_y in range(108, 132):
+                    pygame.draw.rect(screen, (0, 0, 0), (self.width - 119, 108, 118, 24), 1)
+                if self.mouse_y in range(140, 164):
+                    pygame.draw.rect(screen, (0, 0, 0), (self.width - 119, 140, 118, 24), 1)
+                if self.mouse_y in range(172, 196):
+                    pygame.draw.rect(screen, (0, 0, 0), (self.width - 119, 172, 118, 24), 1)
+        screen.blit(img_fundoo, (0,0))
+        #pygame.draw.rect(screen, (255, 213 , 122), (0, 0, self.width - 128, self.height - 10))# fundo dos quadradinhos / # 55 55
+        #screen.blit(img_fundoo, (a * 32, 32 + b * 32))
         for a in range(self.game.m):
             for b in range(self.game.n):
-                
-    
-                if self.game.seenboard[a][b] == 1:          
-                   pygame.display.update(screen.blit(img_spaces[self.game.board[a][b]], (a * 32 + 64 , b * 32 + 32)))
+                if self.game.seenboard[a][b] == 1:
+                    #screen.blit(img_fundoo, (a * 32, 32 + b * 32))
+                    screen.blit(img_spaces[self.game.board[a][b]], (a * 32, 32 + b * 32))  
                 elif self.game.seenboard[a][b] == -1:
-                    pygame.display.update(screen.blit(img_flag, (a * 32 + 64 , b * 32 + 32)))
-                else:
-                    planoFundo = pygame.image.load("resources" + os.sep + "a.png").convert()
-                    pygame.display.update(screen.blit(planoFundo,(a * 32 + 64, 32 + b * 32)))
-
+                    screen.blit(img_flag, (a * 32, 32 + b * 32))
+        for a in range(self.game.m):
+            pygame.draw.line(screen, (123, 255, 32), (a * 32, 40), (a * 32, 32 + self.game.n * 32))  # linha - tracinho - vertica
+        for b in range(self.game.n):
+            pygame.draw.line(screen, (123, 255, 32), (0, 32 + b * 32), (self.game.m * 32, 32 + b * 32))
+        if self.mouse_x in range(0, self.game.m * 34):
+            if self.mouse_y in range(32, 32 + self.game.n * 34):
+                screen.blit(img_marker, ((self.mouse_x // 32) * 30, (self.mouse_y // 32) * 30))
+        pygame.draw.rect(screen, (0, 0, 0), (0, 0, self.width, self.height), 1) # linha - tracinho - horizontal
         pygame.display.flip()
 
-    
+
